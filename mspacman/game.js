26b95f87e5fcc77bb82017118cf699efbc26abcb
Reema Al-Marzoog <reema.almarzoog@gmail.com>
2015-02-26 18:49:41 -0500

    Display blue board and Ms. PacMan

--- mspacman/game.js
+++ mspacman/game.js
@@ -1,1 +1,18 @@
-// Replace this with your code...
+// game_canvas, height: 600, width: 800
+
+function init() {
+	var canvas = document.getElementById("game_canvas");
+	if (canvas.getContext) {
+		ctx = canvas.getContext('2d');
+		var img = new Image();
+		img.addEventListener("load", function() {
+			ctx.drawImage(img, 320, 0, 460, 140, 0, 0, 460, 140);
+			ctx.drawImage(img, 80, 0, 20, 20, 115, 91, 20, 20);
+		}, false);
+
+		img.src = 'pacman10-hp-sprite.png';
+	} else {
+		alert("Sorry; your browser doesn't support canvas!")
+	}
+}
+
