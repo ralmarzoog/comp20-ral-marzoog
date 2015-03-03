a03b2a82c60da036d636389b6a674d1dfdf8bcc0
Reema Al-Marzoog <reema.almarzoog@gmail.com>
2015-03-03 14:45:09 -0500

    Get data from JSON file and display messages on page

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,19 @@
 // Your JavaScript code here...
+
+function parse() {
+    var req = new XMLHttpRequest();
+
+    req.open("GET", "data.json", true);
+    req.onreadystatechange = processData();
+    req.send();
+}
+
+function processData() {
+    if (this.readyState == 4 && this.status == 200) {
+        resp = JSON.parse(this.responseText);
+        
+        for (var i = 0; i < resp.length; i++) {
+            messages.innerHTML = "<p>" + resp[i].content + " - " + resp[i].username + "</p>";
+        }
+    }
+}
