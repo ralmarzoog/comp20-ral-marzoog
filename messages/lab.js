7ba5d90af43c7b9d35a8133e09d6c3398e10378f
Reema Al-Marzoog <reema.almarzoog@gmail.com>
2015-03-05 20:03:51 -0500

    Move parseData func before parse func (debugging)

--- messages/lab.js
+++ messages/lab.js
@@ -1,12 +1,5 @@
 // Your JavaScript code here...
 
-function parse() {
-    request = new XMLHttpRequest();
-
-    request.open("GET", "data.json", true);
-    request.onreadystatechange = parseData();
-    request.send();
-}
 
 function parseData() {
     if (request.readyState == 4 && request.status == 200) {
@@ -18,3 +11,10 @@ function parseData() {
     }
 }
 
+function parse() {
+    request = new XMLHttpRequest();
+
+    request.open("GET", "data.json", true);
+    request.onreadystatechange = parseData();
+    request.send();
+}
